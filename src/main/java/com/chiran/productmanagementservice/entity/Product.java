807f39b775d017edb693755e08eda957224dd014
chiranwettewa <chiranwettewa@gmail.com>
2022-12-11 19:08:13 +0530

    initial commit

--- src/main/java/com/chiran/productmanagementservice/entity/Product.java
+++ src/main/java/com/chiran/productmanagementservice/entity/Product.java
@@ -1,10 +1,11 @@
 package com.chiran.productmanagementservice.entity;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "TBL_PRODUCT")
